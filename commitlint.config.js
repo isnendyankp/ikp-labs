7dff23fefb948f8d5f93c23d211bb851429d7d35
Isnendyan <Isnendyankp@gmail.com>
2026-04-27 16:44:04 +0700

    chore: add commitlint with husky commit-msg hook

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'style', 'docs', 'test', 'chore', 'config'],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'type-empty': [2, 'never'],
+  },
+};
